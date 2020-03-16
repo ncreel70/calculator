b9c6eb7ea46fd90182810bc8cbd041de74864b52
Noah Creel <noah.creel@gmail.com>
2020-03-16 00:49:49 -0600

    added displayOutput function instead of brute force, added basic calulation ability with switch/case

--- script.js
+++ script.js
@@ -1,70 +1,46 @@
-const obj = {
-    number1: () => { 
-        if (document.getElementById('displayOutput').innerHTML === '0') {
-            document.getElementById('displayOutput').innerHTML = '1';
-        } else {
-            document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '1';
-        } 
-        },
-    number2: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '2';
+const display = document.getElementById('displayOutput');
+let preValue = '';
+let postValue = '';
+let mathOperator = '';
+let solution = '';
+let regexNumber = /[^0-9]/g
+
+const numButtonPress = (num) => {
+    if (display.textContent === '0' && mathOperator === ''){
+        preValue = num;
+        display.textContent = preValue;
+    } else if (display.textContent.length < 10 && mathOperator === '') {
+        display.textContent += num;
     } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '2';
-    } 
-        },
-    number3: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '3';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '3';
-    } 
-        },
-    number4: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '4';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '4';
-    } 
-        },
-    number5: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '5';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '5';
-    } 
-        },
-    number6: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '6';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '6';
-    } 
-        },
-    number7: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '7';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '7';
-    } 
-        },
-    number8: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '8';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '8';
-    } 
-        },
-    number9: () => { if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '9';
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '9';
-    } 
-        },
-    number0: () =>{  if (document.getElementById('displayOutput').innerHTML === '0') {
-        document.getElementById('displayOutput').innerHTML = '0'
-    } else {
-        document.getElementById('displayOutput').innerHTML = document.getElementById('displayOutput').innerHTML + '0';
+        postValue = num;
     }
 }
-};
 
-[...document.getElementsByClassName('number')].forEach(function(item) {
-    item.addEventListener('click', function() {
-      obj[this.id]();
-    })
-  })
+const operatorButtonPress = (operator) => {
+    mathOperator = operator;
+    if(postValue != '') {
+        switch (operator) {
+            case '+':
+                solution = preValue + postValue;
+                preValue = solution;
+                display.textContent = solution;
+                break;
+            case '-':
+                solution = preValue - postValue;
+                display.textContent = solution;
+                preValue = solution;
+                break;
+            case '*':
+                solution = preValue * postValue;
+                display.textContent = solution;
+                preValue = solution;
+                break;
+            case '/':
+                solution = preValue / postValue;
+                display.textContent = solution;
+                preValue = solution;
+                break;
 
+        }
+    }
+}
